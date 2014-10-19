3a0e4f1330dd08204999381b1360d7b7a9216067
Sylvain Calador <sylvain.calador@gmail.com>
2014-10-19 21:04:17 +0200

    [REF] Refactoring

--- clabot.py
+++ clabot.py
@@ -6,12 +6,10 @@ import hashlib
 import hmac
 import json
 import pprint
-import os
-import sys
-import requests
 from urllib.parse import unquote
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+import requests
 from requests.auth import HTTPBasicAuth
 from erppeek import Client
 
@@ -28,7 +26,7 @@ class GithubHookHandler(BaseHTTPRequestHandler):
         if digest_type != 'sha1':
             return False
 
-        secret = config['clabot']['token_%s' % repo]
+        secret = config['token_%s' % repo]
         mac = hmac.new(
             bytes(secret, 'utf-8'), msg=data, digestmod=hashlib.sha1)
         return hmac.compare_digest(mac.hexdigest(), signature)
@@ -39,7 +37,7 @@ class GithubHookHandler(BaseHTTPRequestHandler):
         hub_signature = self.headers['X-Hub-Signature']
         github_event = self.headers['X-GitHub-Event']
 
-        if github_event not in self.github_allowed_events:
+        if github_event not in self._github_allowed_events:
             self.send_response(400)
             return
 
@@ -62,7 +60,7 @@ class GithubHookHandler(BaseHTTPRequestHandler):
 
 class PullRequestHandler(GithubHookHandler):
 
-    github_allowed_events = ['pull_request']
+    _github_allowed_events = ['pull_request']
 
     def _have_signed_cla(self, users):
         """
@@ -71,21 +69,21 @@ class PullRequestHandler(GithubHookHandler):
         """
 
         odoo_server = 'http://%s:%i' % (
-            config['clabot']['odoo_host'],
-            int(config['clabot']['odoo_port']),
+            config['odoo_host'],
+            int(config['odoo_port'])
         )
-        odoo_db = config['clabot']['odoo_database']
-        odoo_user = config['clabot']['odoo_user']
-        odoo_password = config['clabot']['odoo_password']
-        odoo_cla_categ = config['clabot']['odoo_cla_categ']
-        odoo_github_login_field = config['clabot']['odoo_github_login_field']
 
-        client = Client(odoo_server, odoo_db, odoo_user, odoo_password)
+        client = Client(
+            odoo_server,
+            config['odoo_database'],
+            config['odoo_user'],
+            config['odoo_password']
+        )
 
         for user in users:
             condition = [
-                (odoo_github_login_field, '=', user),
-                ('category_id', '=', odoo_cla_categ),
+                (config['odoo_github_login_field'], '=', user),
+                ('category_id', '=', config['odoo_cla_categ']),
             ]
             if not client.search('res.partner', condition):
                 return False
@@ -98,8 +96,8 @@ class PullRequestHandler(GithubHookHandler):
             self.send_response(400)
             return
 
-        login = config['clabot']['github_login']
-        password = config['clabot']['github_password']
+        login = config['github_login']
+        password = config['github_password']
 
         owner = event['repository']['owner']['login']
         repo = event['repository']['name']
@@ -125,7 +123,7 @@ class PullRequestHandler(GithubHookHandler):
 
             user = event['pull_request']['user']['login']
 
-            cla_message = config['clabot']['cla_message'].format(user=user)
+            cla_message = config['cla_message'].format(user=user)
             data = {'body': cla_message}
 
             res = requests.post(
@@ -142,9 +140,10 @@ if __name__ == '__main__':
 
     config = configparser.ConfigParser()
     config.read(args.config_file)
+    config = config['clabot']
 
-    interface = config['clabot']['interface']
-    port = int(config['clabot']['port'])
+    interface = config['interface']
+    port = int(config['port'])
 
     server = HTTPServer((interface, port), PullRequestHandler)
 
