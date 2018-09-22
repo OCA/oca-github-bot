b8774450d548fe7b16d07b7f6b2ca6a786dfcc25
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-09-22 19:36:13 +0200

    Improve github interface.
    Add context manager for login and repo access,
    so we can later add a pool.
    
    Add a stub gh_call, to later implement the retry logic
    to handle rate limits.

--- src/oca_github_bot/github.py
+++ src/oca_github_bot/github.py
@@ -1,10 +1,29 @@
 # Copyright (c) ACSONE SA/NV 2018
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
 
+from contextlib import contextmanager
+import logging
+
 import github3
 
 from . import config
 
+_logger = logging.getLogger(__name__)
 
+
+@contextmanager
 def login():
-    return github3.login(token=config.GITHUB_TOKEN)
+    """GitHub login as decorator so a pool can be implemented later."""
+    yield github3.login(token=config.GITHUB_TOKEN)
+
+
+@contextmanager
+def repository(org, repo):
+    with login() as gh:
+        yield gh.repository(org, repo)
+
+
+def gh_call(func, *args, **kwargs):
+    """Intercept GitHub call to wait when the API rate limit is reached."""
+    # TODO
+    return func(*args, **kwargs)
--- src/oca_github_bot/tasks/delete_branch.py
+++ src/oca_github_bot/tasks/delete_branch.py
@@ -4,13 +4,16 @@
 from ..queue import task, getLogger
 
 from .. import github
+from ..github import gh_call
 
 _logger = getLogger(__name__)
 
 
 @task()
-def delete_branch(org, repo, branch):
-    gh = github.login()
-    gh_repo = gh.repository(org, repo)
-    gh_branch = gh_repo.ref(f"heads/{branch}")
-    gh_branch.delete()
+def delete_branch(org, repo, branch, dry_run=False):
+    with github.repository(org, repo) as gh_repo:
+        gh_branch = gh_call(gh_repo.ref, f"heads/{branch}")
+        if dry_run:
+            _logger.info(f"DRY-RUN delete branch {branch} in {org}/{repo}")
+        else:
+            gh_call(gh_branch.delete)
