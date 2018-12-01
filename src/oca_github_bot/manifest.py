516fd00e02ce424a6dbed5283ac63a4bbbb96f2f
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-12-01 16:03:46 +0100

    Add manifest version handling functions
    Towards #24

--- src/oca_github_bot/manifest.py
+++ src/oca_github_bot/manifest.py
@@ -1,9 +1,21 @@
 # Copyright (c) ACSONE SA/NV 2018
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
 
+import ast
 import os
+import re
 
 MANIFEST_NAMES = ("__manifest__.py", "__openerp__.py", "__terp__.py")
+VERSION_RE = re.compile(
+    r"^(?P<series>\d+\.\d+)\.(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)$"
+)
+MANIFEST_VERSION_RE = re.compile(
+    r"(?P<pre>[\"']version[\"']\s*:\s*[\"'])(?P<version>[\d\.]+)(?P<post>[\"'])"
+)
+
+
+class NoManifestFound(Exception):
+    pass
 
 
 def is_addons_dir(addons_dir):
@@ -14,7 +26,56 @@ def is_addons_dir(addons_dir):
         addon_dir = os.path.join(addons_dir, p)
         if not os.path.isdir(addon_dir):
             continue
-        for manifest_name in MANIFEST_NAMES:
-            if os.path.exists(os.path.join(addon_dir, manifest_name)):
-                return True
+        if get_manifest_path(addon_dir):
+            return True
     return False
+
+
+def get_manifest_path(addon_dir):
+    for manifest_name in MANIFEST_NAMES:
+        manifest_path = os.path.join(addon_dir, manifest_name)
+        if os.path.exists(manifest_path):
+            return manifest_path
+    return None
+
+
+def get_manifest(addon_dir):
+    manifest_path = get_manifest_path(addon_dir)
+    if not manifest_path:
+        raise NoManifestFound()
+    with open(manifest_path, "r") as f:
+        return ast.literal_eval(f.read())
+
+
+def set_manifest_version(addon_dir, version):
+    manifest_path = get_manifest_path(addon_dir)
+    with open(manifest_path, "r") as f:
+        manifest = f.read()
+    manifest = MANIFEST_VERSION_RE.sub(r"\g<pre>" + version + r"\g<post>", manifest)
+    with open(manifest_path, "w") as f:
+        f.write(manifest)
+
+
+def bump_version(version, mode):
+    mo = VERSION_RE.match(version)
+    if not mo:
+        raise RuntimeError(f"{version} does not match the expected version pattern.")
+    series = mo.group("series")
+    major = mo.group("major")
+    minor = mo.group("minor")
+    patch = mo.group("patch")
+    if mode == "major":
+        major = int(major) + 1
+    elif mode == "minor":
+        minor = int(minor) + 1
+    elif mode == "patch":
+        patch = int(patch) + 1
+    else:
+        raise RuntimeError("Unexpected bumpversion mode f{mode}")
+    return f"{series}.{major}.{minor}.{patch}"
+
+
+def bump_manifest_version(addon_dir, mode):
+    version = get_manifest(addon_dir)["version"]
+    version = bump_version(version, mode)
+    set_manifest_version(addon_dir, version)
--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -1,7 +1,16 @@
 # Copyright (c) ACSONE SA/NV 2018
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
 
-from oca_github_bot.manifest import is_addons_dir
+import pytest
+from oca_github_bot.manifest import (
+    NoManifestFound,
+    bump_manifest_version,
+    bump_version,
+    get_manifest,
+    get_manifest_path,
+    is_addons_dir,
+    set_manifest_version,
+)
 
 
 def test_is_addons_dir_empty(tmpdir):
@@ -13,3 +22,51 @@ def test_is_addons_dir_one_addon(tmpdir):
     p = tmpdir.mkdir("addon").join("__manifest__.py")
     p.write("{'name': 'addon'}")
     assert is_addons_dir(str(tmpdir))
+
+
+def test_get_manifest_path(tmp_path):
+    addon_dir = tmp_path / "addon"
+    addon_dir.mkdir()
+    assert not get_manifest_path(addon_dir)
+    with pytest.raises(NoManifestFound):
+        get_manifest(addon_dir)
+    manifest_path = addon_dir / "__manifest__.py"
+    with manifest_path.open("w") as f:
+        f.write("{'name': 'the addon'}")
+    assert get_manifest_path(addon_dir) == str(manifest_path)
+    assert get_manifest(addon_dir)["name"] == "the addon"
+
+
+def test_set_addon_version(tmp_path):
+    addon_dir = tmp_path / "addon"
+    addon_dir.mkdir()
+    manifest_path = addon_dir / "__manifest__.py"
+    with manifest_path.open("w") as f:
+        f.write("{'name': 'thé addon', 'version': '1.0.0'}")
+    set_manifest_version(addon_dir, "2.0.1")
+    m = get_manifest(addon_dir)
+    assert m["version"] == "2.0.1"
+    assert m["name"] == "thé addon"
+
+
+def test_bump_version():
+    assert bump_version("12.0.1.0.0", "major") == "12.0.2.0.0"
+    assert bump_version("12.0.1.0.0", "minor") == "12.0.1.1.0"
+    assert bump_version("12.0.1.0.0", "patch") == "12.0.1.0.1"
+    with pytest.raises(RuntimeError):
+        bump_version("1.0", "major")
+    with pytest.raises(RuntimeError):
+        bump_version("1.0.1", "major")
+    with pytest.raises(RuntimeError):
+        bump_version("12.0.1.0.0", "none")
+
+
+def test_bump_manifest_version(tmp_path):
+    addon_dir = tmp_path / "addon"
+    addon_dir.mkdir()
+    manifest_path = addon_dir / "__manifest__.py"
+    with manifest_path.open("w") as f:
+        f.write("{'name': 'the addon', 'version': '12.0.1.0.0'}")
+    bump_manifest_version(addon_dir, "minor")
+    m = get_manifest(addon_dir)
+    assert m["version"] == "12.0.1.1.0"
