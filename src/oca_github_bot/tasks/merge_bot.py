aa58cea21101b52d706ce48a8be352da989eae8e
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-05-29 08:06:20 +0200

    do not push merge bot branch if same as pr
    When the merge bot branch does not
    change compared to the PR branch,
    (ie it is already on the tip of the
    main branch and setup, readme, etc
    are ok then do not push it and merg it
    immediately: there is no need
    to re-test it.

--- src/oca_github_bot/tasks/merge_bot.py
+++ src/oca_github_bot/tasks/merge_bot.py
@@ -12,6 +12,21 @@ from .main_branch_bot import main_branch_bot_actions
 _logger = getLogger(__name__)
 
 
+def _merge_bot_merge_pr(org, repo, merge_bot_branch):
+    pr, target_branch = parse_merge_bot_branch(merge_bot_branch)
+    subprocess.check_call(["git", "checkout", target_branch])
+    # TODO what if is there is a merge conflict anyway here?
+    # TODO what if something else was merged since the bot branch was created
+    subprocess.check_call(["git", "merge", "--no-ff", "--no-edit", merge_bot_branch])
+    subprocess.check_call(["git", "push", "origin", target_branch])
+    subprocess.check_call(["git", "push", "origin", f":{merge_bot_branch}"])
+    with github.login() as gh:
+        gh_pr = gh.pull_request(org, repo, pr)
+        merge_sha = github.git_get_head_sha()
+        github.gh_call(gh_pr.create_comment, f"Merged at {merge_sha}.")
+        github.gh_call(gh_pr.close)
+
+
 @task()
 def merge_bot_start(org, repo, pr, username, bumpversion=None, dry_run=False):
     # TODO error handling
@@ -27,14 +42,22 @@ def merge_bot_start(org, repo, pr, username, bumpversion=None, dry_run=False):
             ["git", "fetch", "origin", f"pull/{pr}/head:{merge_bot_branch}"]
         )
         subprocess.check_output(["git", "checkout", merge_bot_branch])
+        pr_sha = github.git_get_head_sha()
         subprocess.check_output(["git", "rebase", "--autosquash", target_branch])
         # run main branch bot actions
         main_branch_bot_actions(org, repo, target_branch, dry_run)
         if bumpversion:
             for addon in git_modified_addons(".", target_branch):
                 bump_manifest_version(addon, bumpversion, git_commit=True)
-        # push
-        subprocess.check_call(["git", "push", "--force", "origin", merge_bot_branch])
+        if pr_sha != github.git_get_head_sha():
+            # push and let tests run again
+            subprocess.check_call(
+                ["git", "push", "--force", "origin", merge_bot_branch]
+            )
+        else:
+            # nothing changed, no need to retest, merge now
+            # TODO check status or obey order blindly?
+            _merge_bot_merge_pr(org, repo, merge_bot_branch)
 
 
 @task()
@@ -52,6 +75,7 @@ def merge_bot_status_failure(org, repo, merge_bot_branch, sha):
                 gh_pr.create_comment,
                 f"Merge command aborted due to a red status `{merge_bot_branch}`.",
             )
+            # TODO add link to failure status in message?
             # TODO delete merge_bot_branch?
 
 
@@ -63,15 +87,4 @@ def merge_bot_status_success(org, repo, merge_bot_branch, sha):
             # the branch has evolved, this means that this status
             # does not correspond to the last commit of the bot, ignore it
             return
-        pr, target_branch = parse_merge_bot_branch(merge_bot_branch)
-        subprocess.check_call(["git", "checkout", target_branch])
-        subprocess.check_call(
-            ["git", "merge", "--no-ff", "--no-edit", merge_bot_branch]
-        )
-        subprocess.check_call(["git", "push", "origin", target_branch])
-        subprocess.check_call(["git", "push", "origin", f":{merge_bot_branch}"])
-        with github.login() as gh:
-            gh_pr = gh.pull_request(org, repo, pr)
-            merge_sha = github.git_get_head_sha()
-            github.gh_call(gh_pr.create_comment, f"Merged at {merge_sha}.")
-            github.gh_call(gh_pr.close)
+        _merge_bot_merge_pr(org, repo, merge_bot_branch)
