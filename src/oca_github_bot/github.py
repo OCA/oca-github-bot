70b9a0e5ef04594c32b016ab24217e6338c82644
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-08-03 13:07:43 +0200

    rename method for clarity

--- src/oca_github_bot/github.py
+++ src/oca_github_bot/github.py
@@ -142,7 +142,7 @@ def git_push_if_needed(remote, branch, cwd=None):
     return True
 
 
-def git_user_can_push(gh_repo, username):
+def github_user_can_push(gh_repo, username):
     for collaborator in gh_call(gh_repo.collaborators):
         if username == collaborator.login and collaborator.permissions.get("push"):
             return True
--- src/oca_github_bot/tasks/merge_bot.py
+++ src/oca_github_bot/tasks/merge_bot.py
@@ -134,7 +134,7 @@ def merge_bot_start(
     org, repo, pr, username, bumpversion=None, dry_run=False, intro_message=None
 ):
     with github.login() as gh:
-        if not github.git_user_can_push(gh.repository(org, repo), username):
+        if not github.github_user_can_push(gh.repository(org, repo), username):
             gh_pr = gh.pull_request(org, repo, pr)
             github.gh_call(
                 gh_pr.create_comment,
