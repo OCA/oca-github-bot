a92d40c27854f22e35630d0756505b57740a31e0
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-03-21 21:51:52 +0100

    Add a new test for the twine publisher

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -1,8 +1,11 @@
 # Copyright (c) ACSONE SA/NV 2021
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
+import tempfile
+from pathlib import Path
+
 import pytest
 
-from oca_github_bot.pypi import exists_on_index
+from oca_github_bot.pypi import TwineDistPublisher, exists_on_index
 
 
 @pytest.mark.parametrize(
@@ -16,3 +19,19 @@ from oca_github_bot.pypi import exists_on_index
 @pytest.mark.vcr()
 def test_exists_on_index(filename, expected):
     assert exists_on_index("https://pypi.org/simple/", filename) is expected
+
+
+@pytest.mark.vcr()
+def test_twine_publisher_file_exists():
+    """Basic test for the twine publisher.
+
+    This test succeeds despite the bogus upload URL, because the file exists,
+    so no upload is attempted.
+    """
+    publisher = TwineDistPublisher(
+        "https://pypi.org/simple/", "bogus", "username", "password"
+    )
+    with tempfile.TemporaryDirectory() as tmpdir:
+        filepath = Path(tmpdir) / "odoo9_addon_mis_builder-9.0.3.5.0-py2-none-any.whl"
+        filepath.touch()
+        publisher.publish(tmpdir, dry_run=False)
