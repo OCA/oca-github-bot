a44207e95f9cf1b972df5625634513b2e0c14dde
Simone Rubino <sir@takobi.online>
2022-10-11 11:09:11 +0200

    [ADD] migration_pr_check: Remind migration guidelines

--- src/oca_github_bot/webhooks/__init__.py
+++ src/oca_github_bot/webhooks/__init__.py
@@ -7,6 +7,7 @@ from . import (
     on_pr_green_label_needs_review,
     on_pr_open_label_new_contributor,
     on_pr_open_mention_maintainer,
+    on_pr_open_migration_check,
     on_pr_review,
     on_push_to_main_branch,
     on_status_merge_bot,
